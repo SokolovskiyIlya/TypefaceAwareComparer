f83492af5ecc739b5a06d0aa15524f029b7f91f8
Ilya Sokolovskiy <sokolovskiyii@bcs.ru>
2024-06-19 12:36:22 +0300

    init commit

---
+++ TypefaceAwareComparer/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace TypefaceAwareComparer
+{
+    public static class Comparer
+    {
+        private static readonly Dictionary<char, char> _map = new Dictionary<char, char>
+        {
+            {'А', 'A'}, {'В', 'B'}, {'С', 'C'}, {'Е','E'}, {'Н', 'H'}, {'I','1'}, {'l','1'}, {'К','K'}, {'М', 'M'}, {'О', 'O'}, {'0','O'}, {'Р','P'}, {'Т', 'T'},
+            {'Х','X'}, {'У','Y'}, {'а','a'}, {'с','c'}, {'е','e'}, {'п','n'}, {'о','o'}, {'р','p'}, {'и','u'}, {'х','x'}, {'у','y'}, {'З','3'}, {'б','6'}
+        };
+
+        private static string Normalize(string input)
+        {
+            var normalized = new StringBuilder(input.Length);
+            foreach (var c in input)
+            {
+                normalized.Append(_map.TryGetValue(c, out var normalizedChar) ? normalizedChar : c);
+            }
+            return normalized.ToString();
+        }
+
+        public static bool CompareStrings(string str1, string str2)
+        {
+            return Normalize(str1) == Normalize(str2);
+        }
+    }
+}
